f6b76506ee38e57f5b4800e83ad2b4c14a03776f
fernando19z <88866652+fernando19z@users.noreply.github.com>
2021-10-15 22:28:21 -0700

    asset

---
+++ practica 5/Assets/Editor/Migration/factoriales.py
@@ -0,0 +1,13 @@
+import funfac
+
+n=int(input("Ingrese un numero:"))
+
+resultado=funfac.factorial(n)
+
+print("el resultado es:",resultado)
+
+lista=[5,4,20,75,12]
+
+listaf=funfac.faclista(lista)
+
+print(listaf)
